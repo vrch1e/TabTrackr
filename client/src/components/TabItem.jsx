d3f8d4d0ce6889c75814e4583fb075640e310424
archiemt <archiemt7@gmail.com>
2025-02-27 18:06:49 +0000

    homepage UI

---
+++ client/src/components/TabItem.jsx
@@ -0,0 +1,12 @@
+import './TabItem.css'
+
+export default function TabItem({website}) {
+
+    return (
+        <>
+        <div id='item-container'>
+            <h4>{website}</h4>
+        </div>
+        </>
+    )
+}
