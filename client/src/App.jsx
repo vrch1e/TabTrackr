e83ecdef6ec728c058203592a9534e0efeaff209
archiemt <archiemt7@gmail.com>
2025-03-04 15:02:02 +0000

    all planned functionality works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,28 +4,29 @@ import TabList from './components/TabList';
 import './App.css'
 
 function App() {
-  const [tabsToday, setTabsToday] = useState([])
+  const [tabsToday, setTabsData] = useState([])
+  const [selectedPeriod, setSelectedPeriod] = useState('today')
 
   useEffect(() => {
     const fetchSites = async () => {
-      const data = await services.getSites('today')
+      const data = await services.getSites(selectedPeriod)
       console.log('data: ', data)
-      setTabsToday(data)
+      setTabsData(data)
       console.log('re-rendered')
     }
     fetchSites()
     const intervalId = setInterval(fetchSites, 10000)
     return () => clearInterval(intervalId)
 
-  }, [])
+  }, [selectedPeriod])
 
   return (
     <>
     <div id='container'>
       <div id='dashboard'>
-        <h1>Time Tracked Today</h1>
-        <button>Week</button>
-        <button>Month</button>
+        <h1>Time Tracked: {selectedPeriod}</h1>
+        <button onClick={() => {setSelectedPeriod('week')}}>Week</button>
+        <button onClick={() => {setSelectedPeriod('month')}}>Month</button>
         <button id='manage-btn'>Manage Sites</button>
       </div>
       <TabList tabs={tabsToday} />
