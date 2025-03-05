7b19dc14d4fdcb1db908d58493aa23eed789f8f5
archiemt <archiemt7@gmail.com>
2025-03-05 11:44:42 +0000

    git crime

--- server/config/database.js
+++ server/config/database.js
@@ -2,8 +2,6 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv'
 dotenv.config()
 
-console.log(process.env.DB_PASSWORD, process.env.DB_USERNAME)
-
 const sequelize = new Sequelize('TabTrackr', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres',
