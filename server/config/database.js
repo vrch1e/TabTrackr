6fdec1f3840db6ff9eab8198c9643d4f7bc0bfc1
archiemt <archiemt7@gmail.com>
2025-03-05 11:39:40 +0000

    .env variables

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,9 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv'
 dotenv.config()
 
-const sequelize = new Sequelize('TabTrackr', 'postgres', 'OrangeFire393.', {
+console.log(process.env.DB_PASSWORD, process.env.DB_USERNAME)
+
+const sequelize = new Sequelize('TabTrackr', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres',
     logging: false,
