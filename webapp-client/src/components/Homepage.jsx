c0ab157a26393597d838d74334b3df1101803e51
archiemt <archiemt7@gmail.com>
2025-11-26 15:42:21 +0000

    logic transferable across components

--- webapp-client/src/components/Homepage.jsx
+++ webapp-client/src/components/Homepage.jsx
@@ -1,8 +1,42 @@
 import './Homepage.css'
 import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function Homepage() {
     const [search, setSearch] = useState('')
+
+    const authToken = useSelector((state) => state.auth.token)
+    console.log('app.jsx rendered')
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        console.log('authToken from redux: ', authToken);
+
+        function listener(event) {
+            if (event.data.type !== 'RECEIVE_AUTH_TOKEN') return;
+            const token = event.data.token;
+            console.log('token received from content_script: ', token);
+            dispatch(updateToken(token));
+        }
+
+        function contentScriptListener(event) {
+            if (event.data.type !== "CONTENT_SCRIPT_RUNNING") return;
+            window.postMessage({ type: "REQUEST_AUTH_TOKEN" });
+        }
+
+        window.addEventListener("message", listener);
+        window.addEventListener("message", contentScriptListener)
+
+        return () => {
+            window.removeEventListener("message", listener)
+            window.removeEventListener("message", contentScriptListener)
+        };
+
+    }, [])
+
+    useEffect(() => {
+        console.log("AUTH TOKEN UPDATED:", authToken);
+    }, [authToken]);
 
     return (
         <>
@@ -20,6 +54,7 @@ export default function Homepage() {
                     <input id="websearch" type="text" placeholder="Search Websites..." />
                 </div>
             </div>
+            <h1>{authToken}</h1>
         </div>
         </>
     )
